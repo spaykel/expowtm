f631f21bfd20f2f6e876de81285bac3c8f5d7c56
fxtmiguel <miguel.coronado929@icloud.com>
2024-10-29 14:03:36 +0100

    barProfileScreen

--- app/(tabs)/(screens)/barProfile.tsx
+++ app/(tabs)/(screens)/barProfile.tsx
@@ -1,34 +1,93 @@
-import { Image, View, StyleSheet, Platform, Text, TextInput } from 'react-native';
-import { useState } from 'react';
-import ImageViewer from "@/components/imageView";
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
-
-
-export default function barProfile() {
-  const [searchQuery, setSearchQuery] = useState('');
-
+const barProfile = () => {
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.backButton}>
+        <Ionicons name="arrow-back" size={20} color="black" />
+      </TouchableOpacity>
+
       <Image
-        source={require('@/assets/images/WTM-Logo.png')}
-        style={styles.WTMLogo}
+        style={styles.image}
+        source={require('@/assets/images/ontherox.png')}
       />
+      <Text style={styles.text}>
+        Give a Rating
+      </Text>
+      
+      <View style={styles.stars}>
+        <MaterialIcons name ="star-border" size={30} style={styles.starUnselected}></MaterialIcons>
+        <MaterialIcons name ="star-border" size={30} style={styles.starUnselected}></MaterialIcons>
+        <MaterialIcons name ="star-border" size={30} style={styles.starUnselected}></MaterialIcons>
+        <MaterialIcons name ="star-border" size={30} style={styles.starUnselected}></MaterialIcons>
+        <MaterialIcons name ="star-border" size={30} style={styles.starUnselected}></MaterialIcons>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Get Directions</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Leave a Review</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#000000',
   },
-  WTMLogo: {
-    height: 78,
-    width: 190,
-    bottom: 720,
-    left: -50,
+  backButton: {
     position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+  image: {
+    width: '90%',
+    height: 250,
+    marginVertical: 20,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flex: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '80%',
+    marginTop: 70,
+  },
+  stars: {
+    paddingTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: '#6200EE',
+    padding: 12,
+    borderRadius: 8,
+    width: '45%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#000000',
+    fontSize: 16,
+  },
+  starUnselected: {
+    color: '#aaa',
+  },
+  text: {
+    color: '#aaa',
+    paddingTop: 10,
   },
 });
+
+export default barProfile;
